f4c3be2b03b42bb451903b737ca6468a79f553dd
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-12 16:12:19 +0100

    Created base window, added assets

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,108 @@
+// Included Libraries
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <string>
+// end libraries
+
+// Entry point for the program
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// --------------------------------------
+	// Game Setup
+	// --------------------------------------
 
-	while (window.isOpen())
+	// Render Window creation
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Quick Draw",
+		sf::Style::Titlebar | sf::Style::Close);
+
+	// Timer functionality
+	float signalTimeLowerLimit = 5.0f;
+	float signalTimeUpperLimit = 10.0f;
+	sf::Time timeTilSignal = sf::seconds(0.0f);
+	sf::Time timeSinceSignal = sf::seconds(0.0f);
+	sf::Clock gameClock;
+
+	// Create Button Sprite
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+	buttonSprite.setPosition(
+		gameWindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+		gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2
+	);
+
+	// --------------------------------------
+	// end game setup
+	// --------------------------------------
+
+
+
+	// --------------------------------------
+	// Game Loop
+	// --------------------------------------
+	// Runs every frame until the game window is closed
+	while (gameWindow.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+		// --------------------------------------
+		// Input
+		// --------------------------------------
 
+		sf::Event gameEvent;
+		// Process events
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			// Check if the event is the closed event
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				// Close the game window
+				gameWindow.close();
+			} // end Closed event
+
+		} // end input polling while loop
+
+		// --------------------------------------
+		// end Input
+		// --------------------------------------
+
+
+		// --------------------------------------
+		// Update
+		// --------------------------------------
+
+		sf::Time frameTime = gameClock.restart();
+
+		// --------------------------------------
+		// end Update
+		// --------------------------------------
+
+
+
+		// --------------------------------------
+		// Draw
+		// --------------------------------------
+
+		// Clear the window to a single colour
+		gameWindow.clear(sf::Color::Black);
+
+		// Draw everything
+		gameWindow.draw(buttonSprite);
+
+		// Display the window contents on the screen
+		gameWindow.display();
+
+		// --------------------------------------
+		// end Draw
+		// --------------------------------------
+
+
+	} // end of game while loop
+	// --------------------------------------
+	// end game loop
+	// --------------------------------------
+
+
+	// exit point for the program
 	return 0;
-}
+} // end of main() function
